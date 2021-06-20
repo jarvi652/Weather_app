0d2adb0fdbad21d6829440d4e2c53bed992a6890
jarvi652 <jarviskatana@gmail.com>
2021-06-20 18:01:31 +0300

    last

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
-let button = document.querySelector('.btn')
-button.addEventListener('click', (event)=>{
-    event.preventDefault()
-    console.log('jay')
-})
-let content = document.querySelector('.in')
 
-function Seeme(){
-    console.log('hi')
-}
+const form = document.querySelector('.forms')
+const list = document.querySelector('.cities')
+const msg =document.querySelector('.msg')
+const input =  document.querySelector('input')
+const key ='236d5bc0156e0cb50546f3210734d65a'
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    let inputVal = input.value
+    const listItems = list.querySelectorAll(".ajax-section .city")
+    const listItemsArray = Array.from(listItems)
+    if (listItemsArray.length > 0) {
+        const filteredArray = listItemsArray.filter(el => {
+        let content = "";
+        if (inputVal.includes(",")) {
+            if (inputVal.split(",")[1].length > 2) {
+            inputVal = inputVal.split(",")[0];
+            content = el
+                .querySelector(".city-name span")
+                .textContent.toLowerCase();
+            } else {
+            content = el.querySelector(".city-name").dataset.name.toLowerCase();
+            }
+        } else {
+            content = el.querySelector(".city-name span").textContent.toLowerCase();
+        }
+        return content == inputVal.toLowerCase();
+        })
+        if (filteredArray.length > 0) {
+        msg.textContent = `You already know the weather for ${
+            filteredArray[0].querySelector(".city-name span").textContent
+        } ...otherwise be more specific by providing the country code as well 😉`;
+        form.reset();
+        input.focus();
+        return;
+        }
+    }
+    //Ajax here   
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${key}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+            const { main, name, sys, weather } = data;
+            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`
+            const li = document.createElement("li")
+            li.classList.add("city");
+            const markup = `
+                <h2 class="city-name" data-name="${name},${sys.country}">
+                    <span>${name}</span>
+                    <sup>${sys.country}</sup>
+                </h2>
+                <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup>
+                </div>
+                <figure>
+                    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+                    <figcaption>${weather[0]["description"]}</figcaption>
+                </figure>
+                `
+                li.innerHTML = markup
+                list.appendChild(li);
+    })
+        .catch(() => {
+            msg.textContent = "Please search for a valid city 😩"
+        })
+    msg.textContent = "";
+    form.reset();
+    input.focus()
+
+})
