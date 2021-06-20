37501de7a1aa0cd8e88fde87047ceff67d1e9cd4
jarvi652 <jarviskatana@gmail.com>
2021-06-20 05:48:58 +0300

    script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Weather_app
+this project was built as a fullfilment of the odin project curriculum working with API's lesson.
---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('.btn')
+button.addEventListener('click', (event)=>{
+    event.preventDefault()
+    console.log('jay')
+})
+let content = document.querySelector('.in')
+
+function Seeme(){
+    console.log('hi')
+}
